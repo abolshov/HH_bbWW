1151d674dafe78f9711f99e7826d7069ea05060c
Artem Bolshov <abolshov@cern.ch>
2025-05-05 22:07:54 +0200

    almost implement run

--- Analysis/PayloadProducers.py
+++ Analysis/PayloadProducers.py
@@ -2,10 +2,22 @@ from Studies.HME.new.hmeVariables import GetHMEVariables
 
 class HMEProducer:
     def __init__(self, cfg):
-        pass
-
-    def initialize(self):
-        pass
+        self.cfg = cfg
 
     def run(self, dfw):
-        pass
+        df_selected = dfw
+        df_not_selected = dfw
+        if self.cfg['channel'] == "DL":
+            df_selected = df_selected.Filter(f"ncentralJet >= 2 && lep1_pt > 0.0 && lep2_pt > 0.0", "selected_for_HME")
+            df_not_selected = df_selected.Filter(f"!(ncentralJet >= 2 && lep1_pt > 0.0 && lep2_pt > 0.0)", "selected_for_HME")
+        elif self.cfg['channel'] == "SL":
+            df_selected = df_selected.Filter(f"ncentralJet >= 4 && lep1_pt > 0.0", "selected_for_HME")
+            df_not_selected = df_selected.Filter(f"!(ncentralJet >= 4 && lep1_pt > 0.0)", "selected_for_HME")
+        
+        df_not_selected = df_not_selected.Define("hme_mass", "return -1.0f;")
+        df_selected = GetHMEVariables(df_selected, self.cfg['channel'])
+
+        # need to merge df_selected and df_not_selected into single dataframe
+        # how??
+
+        return dfw
